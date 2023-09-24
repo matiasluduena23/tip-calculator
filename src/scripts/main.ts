af4dee28b53487f838bbc8b7589392cccb8af06e
matias luduena <matiasluduena23@gmail.com>
2023-09-24 20:04:39 -0300

    add zod validation for inputs

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,73 +1,117 @@
 import { z } from "zod";
 
+
 const FormSchema =  z.object({
-    inputBill : z.number().gt(0, "Only allow number greater than 0"),
-    inputPeople: z.number().optional(),
-    tipAmountElement : z.string().optional(),
-    totalElement : z.string().optional()
+    inputBill : z.coerce.number().gt(0, "Only allow number greater than 0"),
+    inputPeople: z.coerce.number().gt(0, "Only allow number greater than 0")
 })
 
-const result = FormSchema.safeParse({inputBill: Number(document.querySelector('.bill').value)})
-if(result.success){
-    console.log(result)
-}else{
-    const error =result.error.flatten()
-    console.log(error.fieldErrors.inputBill[0])
+type Input = z.infer<typeof FormSchema>
+
+function getInputs(): Input{
+    const inputB:number = Number(document.querySelector('.bill')!.value);
+    const inputP:number = Number(document.querySelector('.people')!.value);
+    return  {inputBill: inputB, inputPeople: inputP}
 }
 
-// const inputBill = document.querySelector('.bill').value
-// const inputPeople = document.querySelector('.people').value
-// const tipAmountElement = document.querySelector('.tip-amount')
-// const totalElement = document.querySelector('.total')
+function validation(inputs: object){
+   const result = FormSchema.safeParse(inputs);
+    return result;
+}
 
 
 
-// const btns = document.querySelectorAll('.btn')
-
-// btns.forEach((element, index) => {
-//     element.addEventListener('click', () => {
-//        const porcentage = switchPorcentaje(index)
-//        printHtml(porcentage)
-//        removeClass(btns)
-//         element.classList.add('active')
-//      })
-
-// });
 
 
-// function printHtml(porcentage) {
-//     let totalTip = ( porcentage/ 100) * Number(inputBill)
-//     let total = totalTip + Number(inputBill)
-//     let totalPerson = total / Number(inputPeople)
-//     let totalTipPerson = totalTip / Number(inputPeople)
-
-//     tipAmountElement.innerHTML = totalTipPerson.toString();
-//     totalElement.innerHTML = totalPerson.toString();
-// }
 
 
-// function switchPorcentaje(element) {
-//     let porcentage;
-//     switch (element) {
-//         case 0:
-//             porcentage = 5;
-//             break;
-//         case 1:
-//             porcentage = 10;
-//             break;
-//         case 2:
-//             porcentage = 15;
-//             break;
-//         case 3:
-//             porcentage = 25;
-//             break;
-//         default:
-//             porcentage = 50;
-//             break;
-//     }
-//     return Number(porcentage);
-// }
+const btns = document.querySelectorAll('.btn')
 
-// function removeClass(btns) {
-//     btns.forEach(e => e.classList.remove('active'))
-// }
+btns.forEach((element, index) => {
+    element.addEventListener('click', () => {
+            
+        if(validation(getInputs()).success) {
+            const getResult = calcTotal(switchPorcentaje(index), getInputs());
+            printHtml(getResult);
+            element.classList.add('active');
+            
+        }else {
+            console.log(validation(getInputs()).error.ZodError)
+            reset()
+        }
+
+     })
+
+});
+
+
+
+
+
+
+
+
+function printHtml(inputs: array) {
+    removeClass();
+    const tipAmountElement = document.querySelector('.tip-amount')
+    const totalElement = document.querySelector('.total')
+    tipAmountElement!.innerHTML = inputs[1].toString();
+    totalElement!.innerHTML = inputs[0].toString();
+}
+
+
+function calcTotal(porcentage, inputs){
+    let totalTip = ( porcentage/ 100) * inputs.inputBill
+    let total = totalTip + inputs.inputBill
+    let totalPerson = total / inputs.inputPeople
+    let totalTipPerson = totalTip / inputs.inputPeople
+
+    return [totalPerson, totalTipPerson];
+}
+
+function reset(){
+    removeClass()
+    const tipAmountElement = document.querySelector('.tip-amount').innerHTML = "0";
+    const totalElement = document.querySelector('.total').innerHTML = "0";
+
+}
+
+function switchPorcentaje(element: number) {
+    let porcentage: number;
+    switch (element) {
+        case 0:
+            porcentage = 5;
+            break;
+        case 1:
+            porcentage = 10;
+            break;
+        case 2:
+            porcentage = 15;
+            break;
+        case 3:
+            porcentage = 25;
+            break;
+        default:
+            porcentage = 50;
+            break;
+    }
+    return porcentage;
+}
+
+
+// Error clases
+function removeClass() {
+    const btns = document.querySelectorAll('.btn')
+    btns.forEach(e => e.classList.remove('active'))
+}
+
+function addErrorClass(inputElement: string){
+    let errorElement = inputElement?.previousElementSibling;
+    
+    errorElement.style.visibility = 'visible';
+}
+
+function removeErrorClass(inputElement: string){
+    let errorElement = inputElement?.previousElementSibling;
+    errorElement.classList.remove('error');
+}
